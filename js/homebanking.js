be9b4b0831bc481f7aa29b046fe4b11ab5f2885d
Sergio Nicolas Lopez <lopezer.nic@gmail.com>
2017-12-28 09:14:46 -0300

    validaciones en deposito, extraccion y limite

--- js/homebanking.js
+++ js/homebanking.js
@@ -1,7 +1,8 @@
 //Declaración de variables
 nombreUsuario = "López Sergio";
-saldoCuenta = 15000;
+saldoCuenta = 3000;
 limiteExtraccion = 3000;
+
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML
 cargarNombreEnPantalla();
@@ -17,21 +18,30 @@ var restarSaldo = function(dinero){
   saldoCuenta -= dinero;
 }
 
+
 //Funciones que tenes que completar
 function cambiarLimiteDeExtraccion() {
-  nuevoLimite = prompt("Ingresar nuevo límite de extraccion");
+  nuevoLimite = prompt("Ingresar nuevo límite de extracción.");
   limiteExtraccion = nuevoLimite;
   actualizarLimiteEnPantalla();
   alert("Su nuevo límite de extracción es: $ " + limiteExtraccion);
 }
 
 function extraerDinero() {
-  extraccion = prompt("Ingrese la cantidad de dinero que desea extraer");
+  extraccion = prompt("Ingrese la cantidad de dinero que desea extraer.");
   extraccion = parseInt(extraccion);
+  if (extraccion > limiteExtraccion){
+    alert("La cantidad de dinero que deseas extraer es mayor a tu límite de extracción.")
+  } else if (extraccion % 100 !== 0){
+    alert("Solo puedes extraer billetes de 100.");
+  } else if (extraccion > saldoCuenta){
+    alert("No hay saldo disponible en tu cuenta para extraer esa cantidad de dinero.")
+  } else {
   saldoAnterior = saldoCuenta;
   restarSaldo(extraccion);
   actualizarSaldoEnPantalla();
   alert("Saldo anterior: " + saldoAnterior + "\nExtracción: " + extraccion + "\nSaldo actual: " + saldoCuenta);
+}
 }
 
 function depositarDinero() {
